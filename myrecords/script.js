58982ef1338a919cde0b399842cf5e270d87eb9c
RycharDev <rychard.barros.dev@gmail.com>
2023-01-18 21:07:27 -0300

    Add files via upload

---
+++ myrecords/script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('#formRecords')
+const library = new NLWSetup(form)
+
+const date = {
+    study: ['01-23', '01-24', '01-25'],
+    water: ['01-23', '01-24', '01-25'],
+    pedal: ['01-23', '01-24', '01-25']
+}
