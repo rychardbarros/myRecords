67e406a5193f735d1e8c33cf3979e683d2463a2e
RycharDev <rychard.barros.dev@gmail.com>
2023-01-18 22:25:58 -0300

    Update script.js

--- myrecords/script.js
+++ myrecords/script.js
@@ -1,10 +1,11 @@
-const form = document.querySelector('#formRecords')
+const form = document.querySelector("#form-habits")
 const library = new NLWSetup(form)
 
-const date = {
-    study: ['01-23', '01-24', '01-25'],
-    water: ['01-23', '01-24', '01-25'],
-    pedal: ['01-23', '01-24', '01-25']
+const data = {
+  study: ["01-23", "01-24", "01-25", "01-26", "01-27"],
+  water: ["01-23"],
+  pedal: ["01-23"],
 }
 
-library.setData(date)
+library.setData(data)
+library.load()
