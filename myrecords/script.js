f1eb9beae9fa1502d4e1b471162e04b279b69bbb
RycharDev <rychard.barros.dev@gmail.com>
2023-01-19 21:28:53 -0300

    Update script.js

--- myrecords/script.js
+++ myrecords/script.js
@@ -1,11 +1,28 @@
 const form = document.querySelector("#form-habits")
 const library = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  study: ["01-23", "01-24", "01-25", "01-26", "01-27"],
-  water: ["01-23"],
-  pedal: ["01-23"],
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+  
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = library.dayExists(today)
+
+  if (dayExists){
+    alert('Dia já incluso ⛔')
+    return
+  }
+
+  alert('Dia adicionado com sucesso ✔')
+  library.addDay(today)
 }
 
+function save(){
+  localStorage.setItem('records@key', JSON.stringify(library.data))
+}
+
+const data = JSON.parse(localStorage.getItem('records@key')) || {}
 library.setData(data)
 library.load()
